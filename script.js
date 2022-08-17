29787962b365f63b9d8c2bec2c61c6fd65038366
Eric Nao <eric.nao@gmail.com>
2022-08-17 13:18:34 -0700

    Add operate function

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ function multiple(num1, num2) {
 function divide(num1, num2) {
     return num1/num2;
 };
+
+//May want to find a way to accept operator argument as a character rather as a string
+function operate (operator, num1, num2) {
+    if (operator == '+') {
+        return add(num1, num2);
+    } else if (operator == '-') {
+        return subtract(num1, num2);
+    } else if (operator == '*') {
+        return multiple(num1, num2);
+    } else if (operator == '/') {
+        return divide(num1,num2);
+    }
+};
